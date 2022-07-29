c75a71aa44930269b145193c0ac08dd5f00752de
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-07-29 14:57:12 +0100

    jobs routes

---
+++ routes/jobs.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { createJob, deleteJob, getAllJobs, getJob, updateJob } = require('../controllers/jobs');
+
+router.route('/').get(getAllJobs).post(createJob)
+router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
+
+module.exports = router;
